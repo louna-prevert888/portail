c86ac825494aa02c89e6661d64d3a18018173db3
louna-prevert888 <103409948+louna-prevert888@users.noreply.github.com>
2022-06-08 08:50:27 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P14, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P14, 1)
+})
+basic.forever(function () {
+	
+})
