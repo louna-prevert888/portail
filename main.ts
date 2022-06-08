3b70f7a0a3599a0999cfa5c4a4a1da0823a33933
louna-prevert888 <103409948+louna-prevert888@users.noreply.github.com>
2022-06-08 09:15:35 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,28 @@
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P14, 1)
-    basic.showIcon(IconNames.Chessboard)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P14, 0)
     basic.showIcon(IconNames.Ghost)
 })
 basic.forever(function () {
-	
+    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+    }
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        basic.showIcon(IconNames.Heart)
+    } else {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        basic.showIcon(IconNames.TShirt)
+    }
 })
