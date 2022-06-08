d71422241284f5904e76b55870c36a68465d9d7f
louna-prevert888 <103409948+louna-prevert888@users.noreply.github.com>
2022-06-08 08:56:35 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.showIcon(IconNames.Chessboard)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.showIcon(IconNames.Ghost)
 })
 basic.forever(function () {
 	
